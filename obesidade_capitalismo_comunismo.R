ef7c66caf6ff1be692c1bc1fb97b0592b67e13d1
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-03 01:53:40 -0300

    Add files via upload

--- obesidade_capitalismo_comunismo.R
+++ obesidade_capitalismo_comunismo.R
@@ -70,3 +70,23 @@ ggplot(obe1, aes(x = fct_reorder(Entity, media),
               axis_text_size = 14) +
   theme(legend.position = "none",
         axis.text = element_text(color = "black"))
+
+ggplot(obe2, aes(x = Year, y = obesidade_morte,
+                 group = Entity, color = Entity)) +
+  geom_point(shape = 15, size = 2.5) +
+  geom_line(size = 1.2) +
+  scale_color_manual(values = c("#88CCEE", "#CC6677", "#DDCC77",
+                              "#117733", "#332288", "#AA4499"),
+                     labels = c("China", "Cuba", "Alemanha",
+                                "Japão", "Coreia do Norte", "Estados Unidos")) +
+  labs(x = "Tempo (anos)", y = "Porcentagem de mortes 
+                          atribuídas a obesidade",
+       color = "Países") +
+  theme_ipsum(axis_title_size = 16,
+              axis_text_size = 14) +
+  theme(axis.text = element_text(color = "black"))
+
+
+
+
+
