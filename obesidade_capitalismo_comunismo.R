7d9ea08e8dd778063cfea1e1ee8ff103f40e2571
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-02 23:24:27 -0300

    Add files via upload

---
+++ obesidade_capitalismo_comunismo.R
@@ -0,0 +1,22 @@
+
+# Obesidade em países comunistas e capitalistas --------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 02/10/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/obesity -------------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Obesidade é mais comumente medida como índice de Massa Corporal (IMC)
+### A Organização Mundial de Saúde define o IMC como "um simples índice de
+### peso por altura que é comumente usado para classificar abaixo do peso,
+### sobrepeso e obesidade em adultos.
+
+### Os valores de IMC são usados para definir se um indivíduo é considerado
+### para estar abaixo do peso, saudável, cokm sobrepeso ou obeso. A Organização
+### Mundial de Saúde define essas categorias usando os seguintes pontos de
+### corte: IMC entre 25 e 30 é considerado sobrepeso e IMC maior que 30 é
+### definido como obeso.
+
+
+
+
